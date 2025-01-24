1d24b8d3095b2684f2ec231e17b171fc9162acab
Ruthrapathi-murugan <ruthrapathimurugan@outlook.com>
2025-01-24 11:32:29 +0530

    changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import Header from './Header'; // Import the Header component
+import './Home.css';
 import headerImage from '../assets/home.jpg';
 import headerImage2 from '../assets/home2.jpg';
 import headerImage3 from '../assets/home3.jpg';
+import headerImage4 from '../assets/home4.jpg';
 
 const Home = () => {
     return (
-        <div>
+        <div className='home'>
             {/* Header image */}
             <img src={headerImage} alt="Header 1" className="header-img" />
             
@@ -15,6 +16,8 @@ const Home = () => {
             
             {/* Third image */}
             <img src={headerImage3} alt="Header 3" className="header-img" />
+        {/* fourth image */}
+        <img src={headerImage4} alt="Header 3" className="header-img" />
         </div>
     );
 };
